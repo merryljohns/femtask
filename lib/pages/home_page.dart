174bff6d476abb6758f20b709b6b979619cf800b
Avanthika-Sreejith <avanthikasreejith.ixa@gmail.com>
2025-02-02 04:56:11 +0530

    hii

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,21 +8,86 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // text controllers
+  final newExpenseNameController = TextEditingController();
+  final newExpenseAmountController = TextEditingController();
+
+  //add new expense
   void addNewExpense() {
     showDialog(
-      context: context, 
-      builder: (context)=>const AlertDialog(
-        title:Text('Add new expense')
-      ),
-    );
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Add new expense'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  //expense name
+                  TextField(
+                    controller: newExpenseAmountController,
+                  ),
+
+                  //expense amount
+                  TextField(
+                    controller: newExpenseAmountController,
+                  )
+                ],
+              ),
+              actions: [
+                //save button
+                MaterialButton(
+                  onPressed: save,
+                  child: Text('Save'),
+                ),
+                //cancel button
+                MaterialButton(
+                  onPressed: cancel,
+                  child: Text('Cancel'),
+                ),
+              ],
+            ));
   }
+
+  //save
+  void save() {
+    //create expense item
+    ExpenseItem newExpense = ExpenseItem(
+      name: newExpenseNameController.text,
+      amount: newExpenseAmountController.text,
+      dateTime: DateTime.now(),
+    ); //ExpenseItem
+    //add the new expense
+    Provider.of<ExpenseData>(context, listen: false).addNewExpense(newExpense);
+
+    Navigator.pop(context);
+    clear();
+  }
+
+  //cancel
+  void cancel() {
+    Navigator.pop(context);
+  }
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      backgroundColor: Colors.white,
-      floatingActionButton: FloatingActionButton(
-        onPressed:addNewExpense,
-        child: const Icon(Icons.add), 
+  Widget build(BuildContext context) {
+    return Consumer<ExpenseData>(
+      builder: (context, value, child) => Scaffold(
+        backgroundColor: Colors.grey[300],
+        floatingActionButton: FloatingActionButton(
+          onPressed: addNewExpense,
+          child: Icon(Icons.add),
+        ),
+        body: ListView(children: [
+          //weekly summary
+          //expense list
+          ListView.builder(
+            itemCount: value.getAllExpenseList().length,
+            itemBuilder: (context, index) => ExpenseTile(
+              name: value.getAllExpenseList()[index].name,
+              amount: value.getAllExpenseList()[index].amount,
+              dateTime: value.getAllExpenseList()[index].dateTime,
+            ),
+          )
+        ]),
       ),
     );
   }
