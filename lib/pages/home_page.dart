e0d3401b9fc2d3051f4881491e2805098a7686f6
Avanthika-Sreejith <avanthikasreejith.ixa@gmail.com>
2025-02-02 01:56:07 +0530

    fine

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,62 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
     @override
     Widget build(BuildContext context) {
-        return const Scaffold();
+        return Scaffold(
+          appBar: AppBar(
+        title: const Text('My App'),
+        backgroundColor: Colors.pink,
+      ),
+      drawer: const Drawer(
+        child: Column(
+          children: [
+            ListTile(
+              leading: Icon(Icons.money),
+              title: Text('Food expence tinkerspace'),
+              trailing: Icon(Icons.delete),
+            ),
+            ListTile(
+              leading: Icon(Icons.money),
+              title: Text('Food expence tinkerspace'),
+              trailing: Icon(Icons.delete),
+            ),
+            ListTile(
+              leading: Icon(Icons.money),
+              title: Text('Food expence tinkerspace'),
+              trailing: Icon(Icons.delete),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Center(
+              child: Text(
+            'Welcome to Expence Tracker App',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )),
+          const ListTile(
+            leading: Icon(Icons.money),
+            title: Text('Food expence tinkerspace'),
+            trailing: Icon(Icons.delete),
+          ),
+          const ListTile(
+            leading: Icon(Icons.money),
+            title: Text('Food expence tinkerspace'),
+            trailing: Icon(Icons.delete),
+          ),
+          const ListTile(
+            leading: Icon(Icons.money),
+            title: Text('Food expence tinkerspace'),
+            trailing: Icon(Icons.delete),
+          ),
+          Center(
+              child:
+                  FilledButton(onPressed: () {}, child: const Text('Add Expence'))),
+          const Text('666'),
+        ],
+      ),
+        );
     }
   }
 
