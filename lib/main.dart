df90fce1947769e70c74c489a38f3ee164b6f109
merryljohns <merryljohns2006@gmail.com>
2025-02-01 18:46:04 +0530

    chages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TaskInputScreen(),
+      home: const TaskInputScreen(),
     );
   }
   // lll
