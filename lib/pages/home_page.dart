d7949465bae6b5da1e860d861f1d6ca78ea9669b
Avanthika-Sreejith <avanthikasreejith.ixa@gmail.com>
2025-02-02 03:45:36 +0530

    changed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,77 +2,28 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
-
 }
+
 class _HomePageState extends State<HomePage> {
-  void addNewExpense(){
+  void addNewExpense() {
     showDialog(
-      context: context,
+      context: context, 
       builder: (context)=>const AlertDialog(
-        title: Text('add new expense'),
+        title:Text('Add new expense')
       ),
     );
-    }
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-        title: const Text('My App'),
-        backgroundColor: Colors.pink,
-      ),
-      drawer: const Drawer(
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.money),
-              title: Text('Food expence tinkerspace'),
-              trailing: Icon(Icons.delete),
-            ),
-            ListTile(
-              leading: Icon(Icons.money),
-              title: Text('Food expence tinkerspace'),
-              trailing: Icon(Icons.delete),
-            ),
-            ListTile(
-              leading: Icon(Icons.money),
-              title: Text('Food expence tinkerspace'),
-              trailing: Icon(Icons.delete),
-            ),
-          ],
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Center(
-              child: Text(
-            'Welcome to Expence Tracker App',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          )),
-          const ListTile(
-            leading: Icon(Icons.money),
-            title: Text('Food expence tinkerspace'),
-            trailing: Icon(Icons.delete),
-          ),
-          const ListTile(
-            leading: Icon(Icons.money),
-            title: Text('Food expence tinkerspace'),
-            trailing: Icon(Icons.delete),
-          ),
-          const ListTile(
-            leading: Icon(Icons.money),
-            title: Text('Food expence tinkerspace'),
-            trailing: Icon(Icons.delete),
-          ),
-          Center(
-              child:
-                  FilledButton(onPressed: () {}, child: const Text('Add Expence'))),
-          const Text('666'),
-        ],
-      ),
-        );
-    }
   }
-
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed:addNewExpense,
+        child: const Icon(Icons.add), 
+      ),
+    );
+  }
+}
